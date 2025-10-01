f52b3f5b6d12dea534ce9e500e1b6bf3c8b070e1
Fgiannini762 <fede-g@live.it>
2025-10-01 16:55:30 +0200

    uni1 s2/l3

---
+++ UNIT_1/S2/L3/Esercizio_pratica.py
@@ -0,0 +1,6 @@
+città = input("in quale città vivi? ")
+animale = input("quale è il nome del tuo animale domestico? ")
+
+risultato = città + " " + animale
+
+print(f"il nome della tua band è {risultato}")
